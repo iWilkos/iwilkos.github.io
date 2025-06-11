815c5f95735ce0e10146aad3e13746d9130945bc
Ben <benwilkinson02@gmail.com>
2025-06-12 00:33:14 +0100

    CV updated

--- other/programming/PasswordGenerator/obj/Debug/net8.0/PasswordGenerator.AssemblyInfo.cs
+++ other/programming/PasswordGenerator/obj/Debug/net8.0/PasswordGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b87f1813f94b42a65fa5c25951fac1a04a6e539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0d0a840f66e61c6c35a338c5ad8170d3acaa11")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
